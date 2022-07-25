ad59ca46666b7beb8e7f88ec46b06e611e3bb665
GD-Tasmania <luiscontreras20022005@gmail.com>
2022-07-25 01:54:40 -0500

    Se agrega la actividad

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 // 3. ¿Cuál es el porcentaje de mujeres que hay en el grupo?
 // 4. Agregue una foto a cada personaje, y muestrelos en el navegador
 // 5. Cree un nuevo personaje desde el navegador incluyendo imagen.
-
 import{people} from './people.js'
 
 const container = document.getElementById("container")
@@ -13,20 +12,94 @@ const boton2 = document.getElementById("boton2")
 const boton3 = document.getElementById("boton3")
 const boton4 = document.getElementById("boton4")
 const boton5 = document.getElementById("boton5")
+const selectProducts = document.getElementById("select-products")
 
 boton1.addEventListener('click',ejercicio1)
 boton2.addEventListener('click',ejercicio2)
 boton3.addEventListener('click',ejercicio3)
 boton4.addEventListener('click',ejercicio4)
 
-// let name_age = (str) => {
-//     return people.filter((datos) => {
-//         if(datos.name.indexOf(str) != -1) return true
-//         return false
-//     })
-// }
-// console.log(name_age('Jo'))
 
+let imgSelect = " "
+const modal = document.querySelector('.modal')
+const name1 = document.getElementById('new-name')
+const age = document.getElementById('new-age')
+const gender = document.getElementById('new-gender')
+const btnx = document.getElementById('close-modal')
+const imagen1 = document.getElementById('new-image')
+const crear = document.getElementById('new-create')
+
+window.addEventListener("load",listSelect)
+selectProducts.addEventListener('change',renderCards)
+boton5.addEventListener('click', showModal)
+crear.addEventListener('click', createProduct)
+imagen1.addEventListener('change', importImg)
+btnx.addEventListener('click',borrar)
+
+function importImg(event){
+    const traer = event.target.files[0]
+    const objectUrl = URL.createObjectURL(traer)
+    imgSelect = objectUrl
+}
+function createProduct(){
+    const capturarName = name1.value;
+    const capturarAge = age.value;
+    const capturarGender = gender.value;
+    const nuevaPersona = {name: capturarName, age: capturarAge, gender: capturarGender, img: imgSelect}
+    people.push(nuevaPersona)
+    listSelect()
+    modal.style.display = 'none'
+}
+function showModal(){
+    modal.style.display = 'flex'
+}
+function borrar(){
+    modal.style.display = 'none'
+}
+function renderCards (){
+    people.map( personas => {personas.name ===  selectProducts.value ? createCards(personas): null})
+}
+function listSelect(){
+    selectProducts.innerHTML = '' 
+    const anyOption = document.createElement('option')
+    selectProducts.appendChild(anyOption)
+    anyOption.textContent = 'Select a Product'
+    people.map( personas => {
+        const option = document.createElement('option')
+        option.value = personas.name
+        option.textContent = personas.name
+        selectProducts.appendChild(option)
+        })
+}
+
+function createCards(personas){
+    const{name, age, gender, img} = personas
+
+    const card = document.createElement("div")
+    card.classList.add('card-product')
+
+    const imgCard  = document.createElement('img')
+    imgCard.setAttribute('src',img)
+    //imgCard.setAttribute('alt',name)
+    imgCard.classList.add('img-product')
+
+    const nameCard = document.createElement('p')
+    nameCard.textContent = 'Nombre: ' + name
+    nameCard.classList.add('name-product')
+
+    const tipoCard = document.createElement('p')
+    tipoCard.textContent = 'Edad: ' + age
+
+    const ciudadCard = document.createElement('p')
+    ciudadCard.textContent = 'Genero: ' + gender
+
+    card.appendChild(imgCard)
+    card.appendChild(nameCard)
+    card.appendChild(tipoCard)
+    card.appendChild(ciudadCard)
+
+    container.appendChild(card)
+}
 
 function ejercicio1 (){people.filter((hola) => {
     const card = document.createElement('div')
@@ -63,19 +136,6 @@ function ejercicio2 (){people.filter((hola) => {
 })
 }
 
-// let order_age = () => {
-//     return people.reduce((order_age,persona) => {
-//         if (persona.age<=20){
-//             order_age.menor.push(persona)
-//             return order_age
-//         } else {
-//             order_age.mayor.push(persona)
-//         }
-//         return order_age
-//     },{menor:[], mayor:[]})
-// }
-// console.log(order_age());
-
 function ejercicio3() {
     const card = document.createElement('div')
     const porcentaje = document.createElement('p')
@@ -88,12 +148,10 @@ function ejercicio3() {
             y++
         }
     },{})
-    porcentaje.textContent = x*100/(y+x)
+    porcentaje.textContent = (x*100/(y+x)).toFixed(2)+'%'
     card.appendChild(porcentaje)
     container.appendChild(card)
 }
-
-
 
 function ejercicio4(){people.filter((hola) => {
     const card = document.createElement('div')
@@ -102,8 +160,7 @@ function ejercicio4(){people.filter((hola) => {
     const gender = document.createElement('p')
     const imgCard = document.createElement('img')
 
-    imgCard.setAttribute('src',img)
-    imgCard.setAttribute('alt',name)
+    imgCard.setAttribute('src',imgCard)
     imgCard.classList.add('img-product')
 
     card.classList.add('card')
@@ -119,3 +176,13 @@ function ejercicio4(){people.filter((hola) => {
     container.appendChild(card)
 })
 }
+
+
+// [...people,newperson]
+// let name_age = (str) => {
+//     return people.filter((datos) => {
+//         if(datos.name.indexOf(str) != -1) return true
+//         return false
+//     })
+// }
+// console.log(name_age('Jo'))
--- people.js
+++ people.js
@@ -1,5 +1,5 @@
 export const people = [
-    { name: 'John', age: 19, gender: "Male", img:"https://previews.123rf.com/images/anwarsikumbang/anwarsikumbang1502/anwarsikumbang150200446/36649713-hombre-avatar-de-dibujos-animados-imagen-usuario-personaje-ilustraci%C3%B3n-vectorial.jpg"},
+    { name: 'John', age: 19, gender: "Male", img:""},
     { name: 'Jane', age: 21, gender: "Feminine", img:""},
     { name: 'Joe', age: 22, gender: "Male", img:""},
     { name: 'Jack', age: 23, gender: "Male", img:""},
@@ -7,4 +7,3 @@ export const people = [
     { name: 'Juan', age: 25, gender: "Male", img:""},
     { name: 'Jenny', age: 26, gender: "Feminine", img:""},    
 ];
-
